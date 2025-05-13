d2ed58b61de70a3541ec78f19be30bd05587cc1c
DAMLA ARPA <dmlrp2002@gmail.com>
2025-05-13 11:48:15 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace evlendirme_formu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList4 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList5 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList6 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList7 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList8 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList9 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList10 = new System.Windows.Forms.ImageList(this.components);
+            this.ımageList11 = new System.Windows.Forms.ImageList(this.components);
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(374, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "                          EVLENDİRME FORMU";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ADAY İSMİ  :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 57);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(719, 53);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ADAY İSMİ :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ADAY SOYAD :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ADAY SOYAD :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(180, 101);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(719, 98);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 9;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 163);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(221, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Hiç bir baskı altında değilim.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 209);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(299, 24);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Evlenmeye Engel Durumun Bulunmadığı.\r\n";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList3
+            // 
+            this.ımageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList3.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList3.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList4
+            // 
+            this.ımageList4.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList4.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList4.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList5
+            // 
+            this.ımageList5.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList5.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList5.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList6
+            // 
+            this.ımageList6.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList6.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList6.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList7
+            // 
+            this.ımageList7.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList7.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList7.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList8
+            // 
+            this.ımageList8.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList8.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList8.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList9
+            // 
+            this.ımageList9.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList9.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList9.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList10
+            // 
+            this.ımageList10.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList10.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList10.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // ımageList11
+            // 
+            this.ımageList11.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList11.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList11.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(21, 254);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(380, 44);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = " İyi günde kötü günde, hastalıkta ve sağlıkta bir ömür\r\n birlikte olacağımıza söz" +
+    " veriyorum.\r\n";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(544, 163);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(221, 24);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "Hiç bir baskı altında değilim.";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(544, 209);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(299, 24);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.Text = "Evlenmeye Engel Durumun Bulunmadığı.";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(544, 254);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(380, 44);
+            this.checkBox6.TabIndex = 15;
+            this.checkBox6.Text = " İyi günde kötü günde, hastalıkta ve sağlıkta bir ömür\r\n birlikte olacağımıza söz" +
+    " veriyorum.\r\n";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(371, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 58);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "EVLENMEK İÇİN BASIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(412, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::evlendirme_formu.Properties.Resources.WhatsApp_Image_2024_02_22_at_21_511;
+            this.pictureBox1.Location = new System.Drawing.Point(371, 462);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(361, 440);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(207, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(371, 417);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(12, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 545);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private ImageList ımageList1;
+        private ImageList ımageList2;
+        private ImageList ımageList3;
+        private ImageList ımageList4;
+        private ImageList ımageList5;
+        private ImageList ımageList6;
+        private ImageList ımageList7;
+        private ImageList ımageList8;
+        private ImageList ımageList9;
+        private ImageList ımageList10;
+        private ImageList ımageList11;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private Button button1;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label6;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+namespace evlendirme_formu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = (textBox2.Text + textBox4.Text ) + (textBox3.Text + textBox5.Text) + "evlendiniz :) "  ;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
